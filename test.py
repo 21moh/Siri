e48d5c91b9ec2650e4ea480ea4c231b378b8f8de
Matthew Oh <68302300+21moh@users.noreply.github.com>
2023-12-27 02:29:15 -0500

    Delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-from bardapi import Bard
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-token = os.getenv("bwi4O0ymyiaSC1XYwBh9b_4LZHRfmUIUr-OhV9B4hpd5y7PmSoiRE3V8eQ7ds92E6vF-hQ.")
-bard = Bard(token=token)  
-result = bard.get_answer("What is the current stock price of NVDA?")
-print(result)
